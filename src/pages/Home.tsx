2d587c67181167081004a70faa36ebc132760087
David <d.phi.gordon@gmail.com>
2023-02-09 16:40:40 -0600

    added a basic setup for routing, styling,and page layouts

---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+import { Header } from "../shared/components/Header";
+import { PageLayout } from "../shared/components/PageLayout";
+
+export function Home() {
+    return <PageLayout>home...</PageLayout>
+}
