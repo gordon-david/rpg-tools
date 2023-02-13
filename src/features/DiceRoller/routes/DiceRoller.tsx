e298a9cd1aa028dc82258b45255d4d1c878d2102
David <d.phi.gordon@gmail.com>
2023-02-13 15:08:51 -0600

    updated dice mechanics

--- src/features/DiceRoller/routes/DiceRoller.tsx
+++ src/features/DiceRoller/routes/DiceRoller.tsx
@@ -1,26 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Dice } from "../components/Dice";
 import { DicePool } from "../components/DicePool";
-import { DiceTypes } from "../core/Dice";
+import { DiceKey, PolyhedralDicePoolFactory } from "../core/Dice";
 
-interface IDicePool { [key: string]: number}
 
-const defaultDice = (() => {
-    let toReturn = {} as IDicePool;
-    Object.values(DiceTypes.Polyhedral).forEach((dice: string) => toReturn[dice] = 0)
-    return toReturn
-})
-
-export function DiceRoller(){
-    const [dicePool, setDicePool] = useState({ ...defaultDice } as IDicePool)
+export function DiceRoller() {
+    const [dicePool, setDicePool] = useState(PolyhedralDicePoolFactory())
     const [results, setResults] = useState([])
-    
+    const [diceMap, setDiceMap] = useState({})
+
+    useEffect(() => {
+        setDiceMap(dicePool.getDiceMap)
+    }, [dicePool])
+
+    function addDice(key: DiceKey){
+        // add to dicePool
+        dicePool.add(key)
+        setDicePool(dicePool)
+    }
+
+    function removeDice(key: DiceKey){
+        dicePool.remove(key)
+        setDicePool(dicePool)
+    }
+
     return <>
-    <Dice amount={5} diceType={"thing"} displayName={"another"} increment={() => console.log("increment")}/>
-    
-    <div className="dice-roller">
-        <DicePool />
-        <div>Roll Results List</div>
-    </div>
+
+        <div className="dice-roller">
+            <DicePool />
+            <div>Roll Results List</div>
+        </div>
     </>
 }
+
